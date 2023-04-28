eccc12a92cee5b80f94232c41bec0c40f74a44c7
Sebastian Arango <sebastian.x.arango.-nd@disney.com>
2023-04-28 16:45:19 -0500

    Add files for artists

--- app/src/main/java/com/example/vynils/repository/ArtistRepository.kt
+++ app/src/main/java/com/example/vynils/repository/ArtistRepository.kt
@@ -2,13 +2,12 @@ package com.example.vynils.repository
 
 import android.content.Context
 import com.android.volley.Response
-import com.example.vynils.DTO.PerformerDTO
-import com.example.vynils.DTO.ResponseAlbumDTO
+import com.example.vynils.DTO.AlbumArtistDTO
+import com.example.vynils.DTO.ResponseArtistDTO
 import com.example.vynils.brokers.ApiService
 import com.example.vynils.genre.Genre
-import com.example.vynils.model.Album
+import com.example.vynils.model.AlbumArtist
 import com.example.vynils.model.Artist
-import com.example.vynils.model.Performer
 import com.example.vynils.recordlabel.RecordLabel
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
@@ -22,15 +21,31 @@ import kotlin.coroutines.resumeWithException
 class ArtistRepository {
     private val gson = Gson()
 
-    private fun responseArtistToArtist(responseArtist: PerformerDTO): Artist {
-        //val ResponseAlbumDTO = responseArtist.albums.firstOrNull() ?: ResponseAlbumDTO(-1, "Unknown", "", "", "", "", "")
+    private fun responseArtistToArtist(responseArtist: ResponseArtistDTO): Artist {
+        val mainAlbumArtistDTO = responseArtist.albumsArtists.firstOrNull() ?: AlbumArtistDTO(
+            -1,
+            "Unknown",
+            "",
+            "",
+            "",
+            "",
+            ""
+        )
         return Artist(
             id = responseArtist.id,
             name = responseArtist.name,
             image = responseArtist.image,
             description = responseArtist.description,
             birthDate = responseArtist.birthDate,
-
+            albums = AlbumArtist(
+                id = mainAlbumArtistDTO.id,
+                name = mainAlbumArtistDTO.name,
+                cover = mainAlbumArtistDTO.cover,
+                releaseDate = mainAlbumArtistDTO.releaseDate,
+                description = mainAlbumArtistDTO.description,
+                genre = Genre.SALSA,
+                recordLabel = RecordLabel.ELEKTRA
+            )
         )
     }
 
@@ -49,8 +64,9 @@ class ArtistRepository {
             }
         }
 
-        val artistListType: Type = object : TypeToken<List<PerformerDTO>>() {}.type
-        val responseArtists: List<PerformerDTO> = gson.fromJson(responseListener, artistListType)
+        val artistListType: Type = object : TypeToken<List<ResponseArtistDTO>>() {}.type
+        val responseArtists: List<ResponseArtistDTO> =
+            gson.fromJson(responseListener, artistListType)
         responseArtists.map { responseArtistToArtist(it) }
     }
 }
