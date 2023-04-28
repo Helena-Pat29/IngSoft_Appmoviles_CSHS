582c1dba83e2961975c70d7d595705aaba5d0ded
Carlos Tovar <c.tovarb@uniandes.edu.co>
2023-04-28 03:09:38 -0500

    feat: adding album details

--- app/src/main/java/com/example/vynils/ui/album/AlbumDescriptionAdapter.kt
+++ app/src/main/java/com/example/vynils/ui/album/AlbumDescriptionAdapter.kt
@@ -1,80 +1,57 @@
 package com.example.vynils.ui.album
 
-import android.content.Intent
-import android.icu.text.SimpleDateFormat
-import android.os.Build
-import android.text.format.Time
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
-import androidx.annotation.RequiresApi
 import androidx.recyclerview.widget.RecyclerView
-import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.Glide
 import com.example.vynils.R
 import com.example.vynils.model.Album
-import com.squareup.picasso.Picasso
-import com.bumptech.glide.Glide
-import java.time.LocalDateTime
-import java.time.LocalTime
-import java.util.Date
-import java.util.Calendar
 
-
-class AlbumAdapter(private var albums: List<Album>) :
-    RecyclerView.Adapter<AlbumAdapter.AlbumViewHolder>() {
-    private var onClickListener: OnClickListener? = null
+class AlbumDescriptionAdapter(private var albums: Album) :
+    RecyclerView.Adapter<AlbumDescriptionAdapter.AlbumViewHolder>() {
     class AlbumViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         val albumName: TextView = itemView.findViewById(R.id.album_name)
+        val albumDescription: TextView = itemView.findViewById(R.id.album_description)
         val albumCover: ImageView = itemView.findViewById(R.id.album_cover)
+        //val albumYear: TextView = itemView.findViewById(R.id.album_year)
+        val albumDuration: TextView = itemView.findViewById(R.id.track_duration)
         val performerName: TextView = itemView.findViewById(R.id.performer_name)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): AlbumViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.album_item, parent, false)
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.album_description_recycler, parent, false)
         return AlbumViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: AlbumViewHolder, position: Int) {
-        val album = albums[position]
+        val album = albums
         holder.albumName.text = album.name
+        holder.albumDescription.text = album.description
         holder.performerName.text = album.mainPerformer.name
+        //holder.albumYear.text = album.releaseDate
+        holder.albumDuration.text = album.track[1].duration
         Glide.with(holder.itemView.context)
             .load(album.cover)
             .centerCrop()
             .into(holder.albumCover)
-        holder.itemView.setOnClickListener {
-            if (onClickListener != null) {
-                onClickListener!!.onClick(position, album )
-            }
-        }
     }
 
     fun addDuration(album: Album): String {
         val totalDuration = "00:00:00"
         //val ldt = LocalTime.parse(totalDuration)
         //for (track in album.track ){
-            //val ldt1 = LocalTime.parse(track.duration)
+        //val ldt1 = LocalTime.parse(track.duration)
         //}
         return "0"
     }
 
-    override fun getItemCount(): Int = albums.size
+    override fun getItemCount(): Int = 1
 
-    fun updateAlbums(newAlbums: List<Album>) {
+    fun updateAlbums(newAlbums: Album) {
         albums = newAlbums
         notifyDataSetChanged()
-    }
-
-    // A function to bind the onclickListener.
-    fun setOnClickListener(onClickListener: OnClickListener) {
-        this.onClickListener = onClickListener
-    }
-
-    // onClickListener Interface
-    interface OnClickListener {
-        fun onClick(position: Int, model: Album)
     }
 }
