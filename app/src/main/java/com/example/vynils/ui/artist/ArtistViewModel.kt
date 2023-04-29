3ebc1698cacdc70430b290a63e2761990b328e65
Sebastian Arango <sebastian.x.arango.-nd@disney.com>
2023-04-28 20:40:10 -0500

    Add files for artists

--- app/src/main/java/com/example/vynils/ui/artist/ArtistViewModel.kt
+++ app/src/main/java/com/example/vynils/ui/artist/ArtistViewModel.kt
@@ -1,6 +1,7 @@
 package com.example.vynils.ui.artist
 
 import android.app.Application
+import android.util.Log
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
@@ -28,6 +29,7 @@ class ArtistViewModel(application: Application) : AndroidViewModel(application) 
         viewModelScope.launch {
             try {
                 val fetchedArtists = repository.fetchArtists(getApplication())
+                Log.d("log3", fetchedArtists.toString())
                 _artists.value = fetchedArtists
             } catch (e: Exception) {
                 _error.value = e
