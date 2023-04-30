0668de6c40c3c68adbd4fb1e808835be8885cc5a
Helena Patarroyo <h.patarroyo@uniandes.edu.co>
2023-04-29 23:06:40 -0500

    funcional con xml y Description Class

---
+++ app/src/main/java/com/example/vynils/ui/artist/ArtistDescriptionActivity.kt
@@ -0,0 +1,48 @@
+package com.example.vynils.ui.artist
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.MenuItem
+import android.widget.LinearLayout
+import androidx.activity.viewModels
+import androidx.appcompat.widget.Toolbar
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.vynils.R
+import com.example.vynils.model.Album
+import com.example.vynils.model.Artist
+import com.example.vynils.ui.album.AlbumActivity
+import com.example.vynils.ui.album.AlbumViewModel
+
+class ArtistDescriptionActivity : AppCompatActivity() {
+    private val viewModel: AlbumViewModel by viewModels()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_description_artist)
+
+        //var albumList : Album = intent.getParcelableExtra(AlbumActivity.NEXT_SCREEN)!!
+
+        val toolbar: Toolbar = findViewById(R.id.artist_toolbar)
+        setSupportActionBar(toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.title = "Artist Description"
+
+        var recyclerView: RecyclerView = findViewById(R.id.artist_details)
+        //var adapter = ArtistDescriptionActivity(albumList)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        //recyclerView.adapter = adapter
+
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            android.R.id.home -> {
+                onBackPressed()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+}
