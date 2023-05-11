76b10c1a55939a994b65102ff5de1c89371a7931
sriveros21 <s.riverosg@uniandes.edu.co>
2023-05-11 14:26:44 -0500

    Implementacion HUO5 (#45)
    Co-authored-by: Sergio Riveros <sergio.riveros@adidas.com>

---
+++ app/src/main/java/com/example/vynils/repository/CollectorRepository.kt
@@ -0,0 +1,62 @@
+package com.example.vynils.repository
+
+import android.content.Context
+import android.util.Log
+import com.example.vynils.DTO.AlbumArtistDTO
+import com.example.vynils.DTO.ResponseArtistDTO
+import com.example.vynils.DTO.ResponseCollectorDTO
+import com.example.vynils.model.Album
+import com.example.vynils.model.Collector
+import com.example.vynils.model.Comment
+import com.example.vynils.model.Artist
+import com.example.vynils.model.CollectorAlbum
+import com.example.vynils.model.Performer
+import com.example.vynils.network.NetworkServiceAdapter
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import java.lang.reflect.Type
+
+class CollectorRepository {
+    private val gson = Gson()
+
+    fun responseCollectorToCollector(responseCollector: ResponseCollectorDTO): Collector {
+        return Collector(
+            id = responseCollector.id,
+            name = responseCollector.name,
+            telephone = responseCollector.telephone,
+            email = responseCollector.email,
+            comments = responseCollector.comments.map { Comment(it.id, it.description, it.rating) },
+            favoritePerformers = responseCollector.favoritePerformers.map {
+                Performer(
+                    id = it.id,
+                    name = it.name,
+                    image = it.image,
+                    description = it.description,
+                    date = it.birthDate ?: it.creationDate
+                )
+            },
+            collectorAlbums = responseCollector.collectorAlbums.map {
+                CollectorAlbum(
+                    id = it.id,
+                    price = it.price,
+                    status = it.status,
+                )
+            }
+        )
+    }
+
+
+    suspend fun fetchCollectors(
+        context: Context
+    ): List<Collector> = withContext(Dispatchers.IO) {
+        val apiService = NetworkServiceAdapter(context)
+
+        val responseListener = apiService.fetchCollectors()
+        Log.d("ResponseRepository", "API call with $responseListener")
+        val collectorListType: Type = object : TypeToken<List<ResponseCollectorDTO>>() {}.type
+        val responseCollectors: List<ResponseCollectorDTO> = gson.fromJson(responseListener, collectorListType)
+        responseCollectors.map { responseCollectorToCollector(it) }
+    }
+}
